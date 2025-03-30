c218e7e533931f597c1a6f630ccef8b7fdff6be2
sukribot321 <sukrikemce@gmail.com>
2025-03-30 13:22:12 +0700

    Update Tes.py

--- Tes.py
+++ Tes.py
@@ -1,4 +1,6 @@
-import nama
-nama = input ("siapa nama lu?")
-print("oh jadi namalu + nama")
-print("salam kenal ya")
+import tools
+listmenu= [Hack Bumi,Hack Langit,Hack Mars]
+print(PILIH MENU
+0.HACK BUMI
+1.HACK LANGIT
+2.HACK MARS
